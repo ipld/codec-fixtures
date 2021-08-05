7148a6eb2835d7d0396930f6f87be44a0815884d
Rod Vagg <rod@vagg.org>
2021-08-05 14:34:41 +1000

    feat: add dag-pb fixtures
    Closes: https://github.com/ipld/codec-fixtures/issues/1
    
    For Go: pulls in github.com/ipld/go-codec-dagpb, make the codec mapping
    more generic and remove hardwiring of most instances of dagjson
    and dagpb. Now better set up for pulling in arbitrary codecs in both JS
    & Go.

--- go/fixtures.go
+++ go/fixtures.go
@@ -8,23 +8,30 @@ import (
 	"strings"
 
 	"github.com/ipfs/go-cid"
+	_ "github.com/ipld/go-codec-dagpb"
 	"github.com/ipld/go-ipld-prime"
-	"github.com/ipld/go-ipld-prime/codec/dagcbor"
-	"github.com/ipld/go-ipld-prime/codec/dagjson"
+	_ "github.com/ipld/go-ipld-prime/codec/dagcbor"
+	_ "github.com/ipld/go-ipld-prime/codec/dagjson"
 	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
 	basicnode "github.com/ipld/go-ipld-prime/node/basic"
 )
 
+type codecName = string
+
 type codecFixture struct {
+	codec codecName
 	cid   cid.Cid
 	value ipld.Node
 }
 
-type fixtureSet struct {
-	dagcbor codecFixture
-	dagjson codecFixture
-}
+type fixtureSet = map[codecName]codecFixture
 
+var dagPbLp = cidlink.LinkPrototype{Prefix: cid.Prefix{
+	Version:  1,
+	Codec:    0x70, // "dag-pb"
+	MhType:   0x12, // "sha2-256"
+	MhLength: 32,
+}}
 var dagCborLp = cidlink.LinkPrototype{Prefix: cid.Prefix{
 	Version:  1,
 	Codec:    0x71, // "dag-cbor"
@@ -37,11 +44,16 @@ var dagJsonLp = cidlink.LinkPrototype{Prefix: cid.Prefix{
 	MhType:   0x12,   // "sha2-256"
 	MhLength: 32,
 }}
+var codecs = map[codecName]ipld.LinkPrototype{
+	"dag-pb":   dagPbLp,
+	"dag-cbor": dagCborLp,
+	"dag-json": dagJsonLp,
+}
 var linkSystem = cidlink.DefaultLinkSystem()
 
 func loadFixture(dir string) (fixtureSet, error) {
 	files, err := ioutil.ReadDir("../fixtures/" + dir)
-	fixtures := fixtureSet{}
+	fixtures := make(fixtureSet)
 	if err != nil {
 		return fixtures, err
 	}
@@ -61,26 +73,22 @@ func loadFixture(dir string) (fixtureSet, error) {
 		}
 		ext = strings.TrimLeft(ext, ".")
 		na := basicnode.Prototype.Any.NewBuilder()
-		if ext == "dag-json" {
-			err := dagjson.Decode(na, bytes.NewReader(byts))
-			if err != nil {
-				return fixtures, err
-			}
-			fixtures.dagjson = codecFixture{
-				cid:   cid,
-				value: na.Build(),
-			}
-		} else if ext == "dag-cbor" {
-			err := dagcbor.Decode(na, bytes.NewReader(byts))
-			if err != nil {
-				return fixtures, err
-			}
-			fixtures.dagcbor = codecFixture{
-				cid:   cid,
-				value: na.Build(),
-			}
-		} else {
+		lp, ok := codecs[ext]
+		if !ok {
 			fmt.Printf("unknown codec '%v' for fixture '%v'\n", ext, dir)
+		}
+		decoder, err := linkSystem.DecoderChooser(lp.BuildLink(make([]byte, 32)))
+		if err != nil {
+			return fixtures, err
+		}
+		err = decoder(na, bytes.NewReader(byts))
+		if err != nil {
+			return fixtures, err
+		}
+		fixtures[ext] = codecFixture{
+			codec: ext,
+			cid:   cid,
+			value: na.Build(),
 		}
 	}
 	return fixtures, nil
